2bc72c97152f3975ef8c66821a55641bf7b6a0d2
sevser <semyon-evstigneev@yandex.ru>
2019-02-01 20:42:05 +0300

    добавлен vue проект

--- app.py
+++ app.py
@@ -8,5 +8,16 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/test')
+def test_func():
+    return 'thats test page'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+
 if __name__ == '__main__':
     app.run()
